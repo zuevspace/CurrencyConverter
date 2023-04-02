509ddd28bd3488e7dffe3dea577c99f3ee38a052
Алексей Зуев <>
2023-04-02 12:35:45 +0300

    Внес изменнения в программу

--- CurrencyConverter/Program.cs
+++ CurrencyConverter/Program.cs
@@ -1,30 +1,55 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace CurrencyConverter
 {
     class Program
     {
+        public static double currencyAmount;
         static void Main(string[] args)
         {
             Console.WriteLine("Currency Convector 1.0");
-            Console.WriteLine("Что вы хотите поменять?\n1.UDS 2.RUB");
 
-            int from = Int32.Parse(Console.ReadLine());
+            bool work = true;
+            
+            while (work)
+            {
+                Console.WriteLine("Что вы хотите поменять?\n1.UDS 2.RUB");
+            
+                int yourСurrency = Int32.Parse(Console.ReadLine());
+                double oneDollar = 77.32;
 
-            switch (from)
+                ChangeCurrency(yourСurrency, oneDollar);
+                Console.WriteLine("Конверсия валюты завершена!");
+                Console.WriteLine("Напиши \"ВЫХОД\" чтобы выйти");
+                string exit = Console.ReadLine().ToLower();
+
+                if (exit == "выход")
+                {
+                    work = false;
+                }
+                
+            }
+        }
+
+        static void ChangeCurrency(double currency, double dollar)
+        {
+            switch (currency)
             {
                 case 1:
-                    Console.WriteLine("Вы попали в кейс 1");
+                    Console.WriteLine("Напишите вашу сумму UDS которую нужно поменять:");
+                    currencyAmount = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"{Math.Round(currencyAmount * dollar)} RUB");
                     break;
                 case 2:
-                    Console.WriteLine("Вы попали в кейс 2");
+                    Console.WriteLine("Напишите вашу сумму RUB которую нужно поменять:");
+                    currencyAmount = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"{Math.Round(currencyAmount / dollar)} UDS");
                     break;
                 default:
-                    Console.WriteLine("Вы попали последний кейс");
+                    Console.WriteLine("Мы не поняли ваш запрос.");
                     break;
             }
-            
-            Console.WriteLine(from);
         }
     }
 }
